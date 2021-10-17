6656988acf39a5dc78fa49f0b12bc1e06ecd7c53
Alejandro Djukic <alejandrodjukic99@gmail.com>
2021-10-17 15:11:10 -0400

    Creacion de skills

--- main.js
+++ main.js
@@ -41,4 +41,31 @@ carouselSlide.addEventListener('transitionend', () => {
     }
 })
 
+// Skills 
+
+document.querySelector('#boton').addEventListener('click', Datos);
+
+function Datos(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', 'skills.json', true)
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let datos = JSON.parse(this.responseText);
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+            for (let item of datos) {
+                res.innerHTML += `
+                <tr>
+                    <td>${item.title}</td>
+                    <td>${item.level}</td>
+                </tr>
+                `
+            }
+
+        }
+    }
+
+}
 
